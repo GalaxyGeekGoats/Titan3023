03b07973d61e31ca5dd5ba687b209bf8e0ecd1b9
FoosCoding <61320914+FoosCoding@users.noreply.github.com>
2023-10-07 19:47:56 +0200

    building class with parameteres

---
+++ building_class.py
@@ -0,0 +1,10 @@
+class Building
+    def __init__(self, Name, Build_cost, Input, Output, Output_value, Input_value, Build_cost_value)
+        self.Name = Name
+        self.Build_cost = Build_cost
+        self.Input = Input
+        self.Output = Output
+        self.Output_value = Output_value
+        self.Input_value = Input_value
+        self.Build_cost_value = Build_cost_value
+        
