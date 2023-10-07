9781fec83efd22145f977c98706a92df6710fe09
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-10-07 15:16:13 +0200

    use screens

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Select, Static, Button, Label
+from textual.widgets import Header, Footer, Select, Static, Button, Label, Checkbox
 from textual.containers import ScrollableContainer
+from textual.screen import Screen, ModalScreen
+from textual.reactive import reactive
+from textual.binding import Binding
 
 LANGUAGES = ["english", "polski", "deutsch"]
 LANGUAGES = list(map(lambda x: (x, x), LANGUAGES))
 
 
-class Credits(Static):
+class SettingsScreen(Screen):
     def compose(self):
-        yield Label("Authors:")
-        yield Label("Adam, Robert, Witold")
-        yield Button("Back", id="back", variant="default")
-
-    def on_button_pressed(self, event):
-        if event.button.id == "back":
-            self.parent.mount(Menu())
-            self.remove()
-
-class Settings(Static):
-    select = Select(LANGUAGES)
-
-    def compose(self):
-        yield select
+        yield Select(LANGUAGES, id="select_lang")
         yield Button("Change language", id="lang_change", variant="default")
+        yield Checkbox("Is darkmode enabled?", True, id="darkmode")
+        yield Button("Save", id="save_btn", variant="success")
+
+    def on_checkbox_changed(self, event):
+        if event.checkbox.id == "darkmode":
+            self.parent.dark = not self.parent.dark
 
     def on_button_pressed(self, event):
         btn_id = event.button.id
         if btn_id == "lang_change":
-            if select.value == "polski": self.remove()
+            pass
+            # if self.query_one("#select_lang").value  == "polski"
+        elif btn_id == "save_btn":
+            self.app.pop_screen()
 
+class Credits(Screen):
+    def compose(self):
+        yield Header()
+        yield Label("Authors:")
+        yield Label("Adam, Robert, Witold")
+        yield Button("Back", id="back", variant="default")
+        yield Footer()
 
-
+    def on_button_pressed(self, event):
+        if event.button.id == "back":
+            self.dismiss()
 
 class Menu(Static):
     def compose(self):
@@ -38,19 +46,15 @@ class Menu(Static):
         yield Button("New Game", id="new_game", variant="success")
         yield Button("Database", id="database", variant="warning")
         yield Button("Credits", id="credits", variant="default")
-        yield Button("Settings", id="settings", variant="default")
    
-    def __switch_widget(self, widget_class):
-            self.parent.mount(widget_class())
-            self.remove()
-
     def on_button_pressed(self, event):
         btn_id = event.button.id
-        if btn_id in ELEMENTS:
-            self.__switch_widget(ELEMENTS[event.button.id])
+        if btn_id == "credits":
+            self.app.push_screen(Credits())
 
 class Titan3023(App):
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    BINDINGS = [("s", "push_screen('settings')", "Settings")]
+    SCREENS = {"settings": SettingsScreen}
     CSS_PATH = "./style.tcss"
 
     def compose(self):
@@ -58,11 +62,6 @@ class Titan3023(App):
         yield ScrollableContainer(Menu())
         yield Footer()
 
-    def action_toggle_dark(self):
-        self.dark = not self.dark
-
-
-ELEMENTS = {"credits": Credits, "settings": Settings}
 if __name__ == "__main__":
     app = Titan3023()
     app.run()
