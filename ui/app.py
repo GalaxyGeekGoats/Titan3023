d7c2a53b8624c44a11459accf1a62ec0855e05ad
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-10-07 21:53:29 +0200

    disable command palette

--- ui/app.py
+++ ui/app.py
@@ -4,6 +4,7 @@ from ui.menu import Menu
 from ui.quit_screen import QuitScreen
 
 class Titan3023(App):
+    ENABLE_COMMAND_PALETTE = False
     BINDINGS = [("s", "push_screen('settings')", "Settings"), 
                 ("q", "push_screen('quit_screen')", "Quit")]
     SCREENS = {"menu": Menu, "settings": SettingsScreen, "quit_screen": QuitScreen}
