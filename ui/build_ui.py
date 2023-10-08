90fbf9c3e7e0ac3189ceb7cb01e6052924e94dc8
gruszczrob <gruszczrob@gmail.com>
2023-10-08 09:26:11 +0200

    fixed toBuildShop

--- ui/build_ui.py
+++ ui/build_ui.py
@@ -8,8 +8,7 @@ ROWS = [
     ("NAME", "COST", "INPUT", "OUTPUT",)
 
 ]
-i = 0
-while i < 6:
+for i in range(6):
     ROWS.append(
         (str(building_reader.build_building(i).name),
          str(building_reader.build_building(i).build_cost_value) + " x " + str(
@@ -18,31 +17,24 @@ while i < 6:
              building_reader.build_building(i).building_input),
          str(building_reader.build_building(i).output_value) + " x " + str(
              building_reader.build_building(i).building_output)))
-    i += 1
-
-
-TOBUILD =[]
-i = 0
-while i < 6:
-    if building_reader.build_building(i).build_cost == "Iron" and int(building_reader.build_building(i).build_cost_value) <= int(StateSaver.resources.get("iron")):
-        TOBUILD.append(str(building_reader.build_building(i).name))
-    elif building_reader.build_building(i).build_cost == "Silicon" and int(building_reader.build_building(i).build_cost_value) <= int(StateSaver.resources.get("silicon")):
-        TOBUILD.append(str(building_reader.build_building(i).name))
-    elif building_reader.build_building(i).build_cost == "Energy" and int(building_reader.build_building(i).build_cost_value) <= int(StateSaver.resources.get("electricity")):
-        TOBUILD.append(str(building_reader.build_building(i).name))
-    i+=1
-
-TOBUILD = list(map(lambda x: (x, x), TOBUILD))
 
 
 class Build_ui(Screen):
     def compose(self):
+        to_build = []
+        for i in range(6):
+            if building_reader.build_building(i).build_cost == "Iron" and int(
+                    building_reader.build_building(i).build_cost_value) <= int(StateSaver.resources["iron"]):
+                to_build.append((str(building_reader.build_building(i).name), i))
+            elif building_reader.build_building(i).build_cost == "Silicon" and int(
+                    building_reader.build_building(i).build_cost_value) <= int(StateSaver.resources["silicon"]):
+                to_build.append((str(building_reader.build_building(i).name), i))
         yield Header()
         yield Label("Day: " + str(StateSaver.resources["day"]) + "   Iron: " + str(
             StateSaver.resources.get("iron")) + "   Uran: " + str(StateSaver.resources["uran"]) + "   Silicon: " + str(
             StateSaver.resources["silicon"]) + "   Electricity: " + str(StateSaver.resources["electricity"]), id="head")
         yield DataTable()
-        yield Select(TOBUILD, id="select_build")
+        yield Select(to_build, id="select_build")
         yield Button("Build", id="build", variant="default")
         yield Button("Back", id="back", variant="default")
         yield Footer()
@@ -55,6 +47,8 @@ class Build_ui(Screen):
                 Text(str(cell), style="italic #03AC13", justify="right") for cell in row
             ]
             table.add_row(*styled_row)
+
+
 
     def on_button_pressed(self, event):
         btn_id = event.button.id
